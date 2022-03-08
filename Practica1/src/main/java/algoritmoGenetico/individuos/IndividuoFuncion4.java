f6d7a8cbb7ec2b0680a47edecc3e339ee82ec365
Jenniza <35228945+Jenniza@users.noreply.github.com>
2022-03-08 14:26:51 +0100

    funcion 4 y5

--- Practica1/src/main/java/algoritmoGenetico/individuos/IndividuoFuncion4.java
+++ Practica1/src/main/java/algoritmoGenetico/individuos/IndividuoFuncion4.java
@@ -11,29 +11,74 @@ package algoritmoGenetico.individuos;
  */
 public class IndividuoFuncion4 extends Individuo<Boolean>{
 
+    //Michalewicz
     public IndividuoFuncion4(double v,int n) {
         super(v,n);
+        this.tamGenes = new int[n];
+        this.min = new double[n];
+        this.max = new double[n];
+        this.fenotipo = new double[n];
+        int tamTotal =0;
+        for (int i = 0; i < n; i++) {
+            this.min[i] = 0;
+            this.max[i] = Math.PI;
+            this.tamGenes[i] = this.tamGen(this.valorError, min[i], max[i]);
+            tamTotal+=tamGenes[i];
+        }
+       
+      
+        this.cromosoma = new Boolean[tamTotal];
+        for(int i = 0; i < tamTotal; i++) this.cromosoma[i] = this.rand.nextBoolean();
+        for (int i = 0; i < n; i++) {
+            System.out.println("x"+n+getFenotipo(i));
+        }
     }
 
-   
+    @Override
+    public double getValor() {
+        double valor=0;
+       for(int i = 0; i < fenotipo.length;i++) {
+			valor += Math.sin(getFenotipo(i)) *Math.pow((Math.sin(((i+1)*Math.pow(getFenotipo(i),2))/Math.PI)),20);
+       }
+        
+        return valor;
+    }
 
+    @Override
     public double evalua() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public Individuo clon(Boolean [] b) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        IndividuoFuncion4 clon= new IndividuoFuncion4(this.getValorError(),0);
+        clon.setCromosoma(b);
+        clon.evalua();
+        return clon;
     }
 
     @Override
     public double getFenotipo(int num) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        double res=0;
+        int pos=0;
+        int p=0;
+        //ArrayList<T> alelo= new ArrayList();
+        for (int i = 0; i < num; i++) {
+            pos+=tamGenes[i];
+        }
+        for (int j = 0; j < tamGenes[num]; j++) {
+            //alelo.add(cromosoma[j]);
+            double aux=0;
+            if(cromosoma[j+pos]){
+                aux=1;
+            }
 
-    @Override
-    public double getValor() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            res+=aux*Math.pow(2, p);
+            p++;
+
+        }
+
+        return min[num]+res*((max[num]-min[num]))/(Math.pow(2, tamGenes[num])-1);
     }
     
 }
--- Practica1/src/main/java/algoritmoGenetico/individuos/IndividuoFuncion5.java
+++ Practica1/src/main/java/algoritmoGenetico/individuos/IndividuoFuncion5.java
@@ -10,7 +10,7 @@ package algoritmoGenetico.individuos;
  * @author usuario_local
  */
 public class IndividuoFuncion5 extends Individuo<Double>{
-
+    //Michalewicz
     public IndividuoFuncion5(double v,int n) {
         super(v,n);
         this.tamGenes = new int[n];
@@ -35,7 +35,12 @@ public class IndividuoFuncion5 extends Individuo<Double>{
 
     @Override
     public double getValor() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        double valor=0;
+       for(int i = 0; i < fenotipo.length;i++) {
+			valor += Math.sin(getFenotipo(i)) *Math.pow((Math.sin(((i+1)*Math.pow(getFenotipo(i),2))/Math.PI)),20);
+       }
+        
+        return valor;
     }
 
     @Override
@@ -44,13 +49,35 @@ public class IndividuoFuncion5 extends Individuo<Double>{
     }
 
     @Override
-    public Individuo clon(Double[] crom) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Individuo clon(Double[] b) {
+        IndividuoFuncion5 clon= new IndividuoFuncion5(this.getValorError(),0);
+        clon.setCromosoma(b);
+        clon.evalua();
+        return clon;
     }
 
     @Override
     public double getFenotipo(int num) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        double res=0;
+        int pos=0;
+        int p=0;
+        //ArrayList<T> alelo= new ArrayList();
+        for (int i = 0; i < num; i++) {
+            pos+=tamGenes[i];
+        }
+        for (int j = 0; j < tamGenes[num]; j++) {
+            //alelo.add(cromosoma[j]);
+            double aux=0;
+            if(cromosoma[j+pos]>1){
+                aux=1;
+            }
+
+            res+=aux*Math.pow(2, p);
+            p++;
+
+        }
+
+        return min[num]+res*((max[num]-min[num]))/(Math.pow(2, tamGenes[num])-1);
     }
     
 }
